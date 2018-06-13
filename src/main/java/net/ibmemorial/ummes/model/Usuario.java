71b986601a25cab028d233aa571be51df000ac68
José Daniel Oliveira Saraiva <jd.saraiva@gmail.com>
2018-06-13 09:34:22 -0300

    Acerto

--- src/main/java/net/ibmemorial/ummes/model/Usuario.java
+++ src/main/java/net/ibmemorial/ummes/model/Usuario.java
@@ -1,44 +1,40 @@
-/*    */ package net.ibmemorial.ummes.model;
-/*    */ 
-/*    */ import com.google.gwt.user.client.rpc.IsSerializable;
-/*    */ 
-/*    */ public class Usuario implements java.io.Serializable, IsSerializable
-/*    */ {
-/*    */   private String login;
-/*    */   private String nome;
-/*    */   private String senha;
-/*    */   private Integer codigoInscrito;
-/*    */   
-/*    */   public String getLogin()
-/*    */   {
-/* 14 */     return this.login;
-/*    */   }
-/*    */   
-/*    */   public void setLogin(String login) {
-/* 18 */     this.login = login;
-/*    */   }
-/*    */   
-/*    */   public String getNome() {
-/* 22 */     return this.nome;
-/*    */   }
-/*    */   
-/*    */   public void setNome(String nome) {
-/* 26 */     this.nome = nome;
-/*    */   }
-/*    */   
-/*    */   public String getSenha() {
-/* 30 */     return this.senha;
-/*    */   }
-/*    */   
-/*    */   public void setSenha(String senha) {
-/* 34 */     this.senha = senha;
-/*    */   }
-/*    */   
-/*    */   public Integer getCodigoInscrito() {
-/* 38 */     return this.codigoInscrito;
-/*    */   }
-/*    */   
-/*    */   public void setCodigoInscrito(Integer codigoInscrito) {
-/* 42 */     this.codigoInscrito = codigoInscrito;
-/*    */   }
-/*    */ }
+package net.ibmemorial.ummes.model;
+
+public class Usuario implements java.io.Serializable {
+	private String login;
+	private String nome;
+	private String senha;
+	private Integer codigoInscrito;
+
+	public String getLogin() {
+		return this.login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSenha() {
+		return this.senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Integer getCodigoInscrito() {
+		return this.codigoInscrito;
+	}
+
+	public void setCodigoInscrito(Integer codigoInscrito) {
+		this.codigoInscrito = codigoInscrito;
+	}
+}
